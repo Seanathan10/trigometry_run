b007349dcb3254e555284859013b7ed9503b641c
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2022-03-11 14:33:06 -0800

    update

---
+++ GameScripts.js
@@ -0,0 +1,26 @@
+new Audio('/Mundian.mp3').play()
+
+//Create the audio tag
+var soundFile = document.createElement("audio");
+soundFile.preload = "auto";
+
+//Load the sound file (using a source element for expandability)
+var src = document.createElement("source");
+src.src = "Mundian.mp3";
+soundFile.appendChild(src);
+
+//Load the audio tag
+//It auto plays as a fallback
+soundFile.load();
+soundFile.volume = 1.000000;
+soundFile.play();
+
+//Plays the sound
+function play() {
+   //Set the current time for the audio file to the beginning
+   soundFile.currentTime = 0.01;
+   soundFile.volume = volume;
+
+   //Due to a bug in Firefox, the audio needs to be played after a delay
+   setTimeout(function(){soundFile.play();},1);
+}
