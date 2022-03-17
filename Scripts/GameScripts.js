9f722aefac8cd2fcbe3a1026d7a20aaabe2ae10f
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2022-03-17 00:55:49 -0700

    udate

---
+++ Scripts/GameScripts.js
@@ -0,0 +1,21 @@
+var Frame = $( "#GameFrame" )[0];
+
+function ErrorMessage() {
+    alert( "These are controls for mobile devices. Currently, they do not work." );
+}
+
+function ShootFunction() {
+    // alert( "boom boom" );
+    // Frame.contentWindow.focus();
+    // element.dispatchEvent(new KeyboardEvent('keydown', {'key': 'c'}));
+    // console.log( "SHOOT" );
+
+    ErrorMessage();
+}
+
+function JumpFunction() {
+    // alert( "boing boing" );
+    // console.log( "JUMP" );
+
+    ErrorMessage();
+}
